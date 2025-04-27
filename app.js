56c73e3f449a43b30c8317846a306dd485a7508a
Debadrita Dey <debadritadey08@gmail.com>
2025-04-27 16:57:47 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const express = require('express')
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser= require('body-parser')
+const dotenv = require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const cors = require('cors')
+
+app.use(bodyParser.json())
 
 
-app.use(bodyParser.json());
-app.use(cors());
 
-app.post('/getResponse', (req, res) => {
+app.post('/getResponse',(req,res)=>{
     console.log(req.body.question)
-    const genAI = new GoogleGenerativeAI('AIzaSyBMDJmXsX4Yr7xRGsjnFowk3m0yvcaTxaU');
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+    const genAI = new GoogleGenerativeAI('AIzaSyDoO29gVIwr09I4c7WDGntazK49IWV6CMk');
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    
+    
     model.generateContent(req.body.question).then(result=>{
-        console.log(result.response.text());
-        const response = result.response.text();
+        console.log(result.response.text())
+        const response = result.response.text()
         res.status(200).json({
-            response:response
+            response: response
         })
     })
     .catch(err=>{
-        console.log(err);
+        console.log(err)
         res.status(500).json({
-            error:err
+            error: err
         })
     })
-})
 
-app.get('/',(req,res)=>{
+});
+
+app.get('/', (req, res) => {
     res.status(404).json({
-        msg:'bad request'
-    })
-})
+         msg: 'bad request' 
+    });
+});
+
 
 
 module.exports = app;
